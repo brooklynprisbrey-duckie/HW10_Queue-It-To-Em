2c521f767fe2e09c48b32ded0bf406f5d0940877
brooklynprisbrey-duckie <brooklynprisbrey@mail.weber.edu>
2026-03-19 17:40:39 -0600

    UML carried out. Awaiting code, color-coding not working.

--- inc/LinkedList.h
+++ inc/LinkedList.h
@@ -6,5 +6,38 @@
 using namespace std;
 
 //TODO: Create your Node Struct and Queue class here
+struct Node {
+	string data = "hi-ho kermit the frog here";
+	Node* next;
+};
+
+class Queue {
+private:
+	Node* front;
+	Node* back;
+	int count;
+public:
+	Queue();
+	~Queue();
+
+	void enqueue(string data) {
+
+	}
+	void dequeue() {
+
+	}
+	string peek() {
+
+	}
+	bool isEmpty() {
+
+	}
+	int size() {
+
+	}
+	void print() {
+
+	}
+};
 
 #endif
