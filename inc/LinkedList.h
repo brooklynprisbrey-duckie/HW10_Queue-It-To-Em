a01f4a17d63a5e1c44764e4f93fd78fb4368b604
brooklynprisbrey-duckie <brooklynprisbrey@mail.weber.edu>
2026-03-19 18:15:42 -0600

    Trying to get color coding to work, show me code-check!

--- inc/LinkedList.h
+++ inc/LinkedList.h
@@ -13,27 +13,35 @@ struct Node {
 
 class Queue {
 private:
-	Node* front;
-	Node* back;
-	int count;
+	Node* front = nullptr;
+	Node* back = nullptr;
+	int count = 0;
 public:
-	Queue();
-	~Queue();
+	Queue() {
+		front = nullptr;
+		back = nullptr;
+		count = 0;
+	};
+	~Queue() {
+		//delete ???; all data especially called with "new" needs to be deleted especially
+		cout << "Queue destroyed. Rides free. Nothing will stop you now." << endl;
+	};
 
 	void enqueue(string data) {
-
+		//will we use printer arithmatic? Pointer++ as they are all consecutive
+		Node count;
 	}
 	void dequeue() {
 
 	}
 	string peek() {
-
+		return "Kermie!!!";
 	}
 	bool isEmpty() {
-
+		return true;
 	}
 	int size() {
-
+		return 0;
 	}
 	void print() {
 
